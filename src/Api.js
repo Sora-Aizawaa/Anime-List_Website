fa74dc88af2136723bbaace7d8bba93f3bb8eb81
MRA-goosebumps <hazedivision127@gmail.com>
2025-06-15 10:19:14 +0700

    firstCommit

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const getMovieList = async () => {
+  const movie = await axios.get("https://api.jikan.moe/v4/anime");
+  //   console.log({ movieList: movie });
+  return movie.data.data;
+};
+
+export const searchMovie = async (query) => {
+  const search = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}`);
+  return search.data.data;
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getMovieList, searchMovie } from "./Api.js";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [animeList, setAnimeList] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((data) => {
+      setAnimeList(data);
+    });
+  }, []);
+
+  const BestAnimeList = () => {
+    return animeList.map((anime, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{anime.title}</div>
+          <img
+            className="Movie-image"
+            src={anime.images.jpg.image_url}
+            alt="Movie Poster"
+          />
+          <div className="Movie-episode">Episodes: {anime.episodes} Ep</div>
+          <div className="Movie-rate">Anime Score: {anime.score}</div>
+        </div>
+      );
+    });
+  };
+
+  // console.log({ animeList: animeList });
+
+  let debounceTimeout;
+
+  const search = (r) => {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(async () => {
+      if (r.length > 2) {
+        const query = await searchMovie(r);
+        setAnimeList(query);
+      } else {
+        const data = await getMovieList(); // tampilkan semua kalau input kosong
+        setAnimeList(data);
+      }
+    }, 500); // delay 500ms
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ANIME LIST WEBSITE </h1>
+        <input
+          placeholder="Search Movie..."
+          className="Movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="Movie-container">
+          <BestAnimeList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
